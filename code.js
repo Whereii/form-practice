50bc5b5bc4d3cc4191216171ac723201b4878c77
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-06-28 17:23:36 -0400

    Finish creating form and styling rest of page

---
+++ code.js
@@ -0,0 +1,8 @@
+let requiredPassword = document.querySelector('input.password-form');
+let confirmPassword = document.querySelector('input.confirm-password')
+
+
+
+if(confirmPassword.textContent = '') {
+    confirmPassword.style.border-color = "red";
+}
