1ba5a359cfd92e6deb18e758db863ea7adfc994e
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-06-28 18:04:26 -0400

    Add js to create interactivity with the password area

--- code.js
+++ code.js
@@ -1,8 +1,19 @@
-let requiredPassword = document.querySelector('input.password-form');
-let confirmPassword = document.querySelector('input.confirm-password')
+let password = document.getElementById('password-form');
+let passwordConfirm = document.getElementById('confirm-password')
+let passwordNoMatch = document.querySelector('.password-no-match')
 
 
 
-if(confirmPassword.textContent = '') {
-    confirmPassword.style.border-color = "red";
-}
+document.addEventListener('change', function () {
+    if(password.value != passwordConfirm.value) {
+        passwordNoMatch.textContent = "* Passwords do not match";
+        passwordNoMatch.style.color = "red";
+        password.style.borderColor = "red";
+        passwordConfirm.style.borderColor = "red";
+    }
+    if(password.value == passwordConfirm.value) {
+        passwordNoMatch.textContent = "";
+        password.style.borderColor = "black";
+        passwordConfirm.style.borderColor = "black";
+    }
+})
